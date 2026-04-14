74f70a5f3eb67aa0c5bafe17ae685808fcd6c0d3
gabriel.estefono <gabrielestefono@hotmail.com>
2026-04-14 13:04:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <figure>
+      <img src="/top-image.png" alt="Omelete with cheese and ham" />
+    </figure>
+    <h1 class="text-preset-01">Simple Omelette Recipe</h1>
+    <p class="text-preset-04">
+      An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs
+      cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.
+    </p>
+  </header>
+  <main>
+    <article>
+      <section>
+        <h2 class="text-preset-03">Preparation time</h2>
+        <ul>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Total: </strong
+            ><span>Approximately 10 minutes</span>
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Preparation: </strong><span>5 minutes</span>
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Cooking: </strong><span>5 minutes</span>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2 class="text-preset-02">Ingredients</h2>
+        <ul>
+          <li class="text-preset-04">2-3 large eggs</li>
+          <li class="text-preset-04">Salt, to taste</li>
+          <li class="text-preset-04">Pepper, to taste</li>
+          <li class="text-preset-04">1 tablespoon of butter or oil</li>
+          <li class="text-preset-04">
+            Optional fillings: cheese, diced vegetables, cooked meats, herbs
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2 class="text-preset-02">Instructions</h2>
+        <ol>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Beat the eggs: </strong>In a bowl, beat the eggs
+            with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of
+            water or milk for a fluffier texture.
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Heat the pan: </strong>Place a non-stick frying pan
+            over medium heat and add butter or oil.
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Cook the omelette: </strong>Once the butter is
+            melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the
+            surface.
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Add fillings (optional): </strong>When the eggs
+            begin to set at the edges but are still slightly runny in the middle, sprinkle your
+            chosen fillings over one half of the omelette.
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Fold and serve: </strong>As the omelette continues
+            to cook, carefully lift one edge and fold it over the fillings. Let it cook for another
+            minute, then slide it onto a plate.
+          </li>
+          <li class="text-preset-04">
+            <strong class="text-preset-04-bold">Enjoy: </strong>Serve hot, with additional salt and
+            pepper if needed.
+          </li>
+        </ol>
+      </section>
+      <section>
+        <h2 class="text-preset-02">Nutrition</h2>
+        <table>
+          <thead>
+            <tr>
+              <th class="text-preset-04">
+                The table below shows nutritional values per serving without the additional
+                fillings.
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="text-preset-04-bold">Calories</td>
+              <td class="text-preset-04">277kcal</td>
+            </tr>
+            <tr>
+              <td class="text-preset-04-bold">Carbs</td>
+              <td class="text-preset-04">0g</td>
+            </tr>
+            <tr>
+              <td class="text-preset-04-bold">Protein</td>
+              <td class="text-preset-04">20g</td>
+            </tr>
+            <tr>
+              <td class="text-preset-04-bold">Fat</td>
+              <td class="text-preset-04">22g</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </article>
+  </main>
+  <footer></footer>
+</template>
+
+<style scoped></style>
